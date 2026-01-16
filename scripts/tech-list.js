cd19f9dbbd884c491cf918685ecffb498cf5c358
Angie <angiegrandauni@gmail.com>
2026-01-16 20:25:16 +0100

    Initial website project

---
+++ scripts/tech-list.js
@@ -0,0 +1,30 @@
+export let tech = [
+    {
+        name: "C++",
+        image: "../images/cpp-logo.jpg"
+    },
+    {
+        name: "C#",
+        image: "../images/csharp-logo.jpg"
+    },
+    {
+        name: "Python",
+        image: "../images/python-logo.jpg"
+    },
+    {
+        name: "HTML",
+        image: "../images/html-logo.jpg"
+    },
+    {
+        name: "CSS",
+        image: "../images/css-logo.jpg"
+    },
+    {
+        name: "JS",
+        image: "../images/javascript-logo.jpg"
+    },
+    {
+        name: "Java",
+        image: "../images/java-logo.jpg"
+    }
+]
